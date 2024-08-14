45c199636ec7c1a6be22b83321c8bee730dd55b3
bbbang105 <2018111366@dgu.ac.kr>
2024-08-14 15:58:37 +0900

    Initial Commit

---
+++ src/main/java/side/onetime/OnetimeApplication.java
@@ -0,0 +1,13 @@
+package side.onetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnetimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnetimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/side/onetime/OnetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package side.onetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
