fb505b7101d0f56c7727aa968c97b1e5c9141c37
bbbang105 <2018111366@dgu.ac.kr>
2024-08-15 01:51:39 +0900

    [Feat] : Member 엔티티 & 레포지토리 추가

---
+++ src/main/java/side/onetime/domain/Member.java
@@ -0,0 +1,41 @@
+package side.onetime.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import side.onetime.global.common.dao.BaseEntity;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "members")
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "members_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "events_id", foreignKey = @ForeignKey(name = "members_fk_events_id"))
+    private Event event;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "pin", nullable = false)
+    private String pin;
+
+    @OneToMany(mappedBy = "member",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Schedule> schedules;
+
+    @Builder
+    public Member(Event event, String name, String pin) {
+        this.event = event;
+        this.name = name;
+        this.pin = pin;
+    }
+}
