7b373fc82ce350a75626d3ea829a657cc27f6870
bbbang105 <2018111366@dgu.ac.kr>
2024-08-15 19:48:48 +0900

    [Feat] : 요일 스케줄 등록 실패 코드 추가

--- src/main/java/side/onetime/exception/ScheduleErrorResult.java
+++ src/main/java/side/onetime/exception/ScheduleErrorResult.java
@@ -10,6 +10,7 @@ import side.onetime.global.common.dto.ErrorReasonDto;
 @RequiredArgsConstructor
 public enum ScheduleErrorResult implements BaseErrorCode {
     _NOT_FOUND_DAY_SCHEDULES(HttpStatus.NOT_FOUND, "404", "요일 스케줄을 가져오는 데 실패했습니다."),
+    _NOT_FOUND_DATE_SCHEDULES(HttpStatus.NOT_FOUND, "404", "날짜 스케줄을 가져오는 데 실패했습니다."),
     ;
 
     private final HttpStatus httpStatus;
