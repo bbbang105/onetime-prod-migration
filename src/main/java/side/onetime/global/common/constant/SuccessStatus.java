332ca5a1f45227bd66f52e0ccf5aab6db652687e
bbbang105 <2018111366@dgu.ac.kr>
2024-08-15 18:33:48 +0900

    [Feat] : 멤버 로그인 및 중복 확인 성공 코드 추가

--- src/main/java/side/onetime/global/common/constant/SuccessStatus.java
+++ src/main/java/side/onetime/global/common/constant/SuccessStatus.java
@@ -14,7 +14,10 @@ public enum SuccessStatus implements BaseCode {
     _CREATED(HttpStatus.CREATED, "201", "생성에 성공했습니다."),
     // Event
     _CREATED_EVENT(HttpStatus.CREATED, "201", "이벤트 생성에 성공했습니다."),
-    _GET_EVENT(HttpStatus.OK, "200", "이벤트 조회에 성공했습니다.")
+    _GET_EVENT(HttpStatus.OK, "200", "이벤트 조회에 성공했습니다."),
+    // Member
+    _LOGIN_MEMBER(HttpStatus.OK, "200", "멤버 로그인에 성공했습니다."),
+    _IS_POSSIBLE_NAME(HttpStatus.OK, "200", "멤버 이름 중복 확인에 성공했습니다.")
     ;
 
     private final HttpStatus httpStatus;
