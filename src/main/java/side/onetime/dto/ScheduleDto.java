df5339a69794bca3c037a0b9fb369b728af3a019
bbbang105 <2018111366@dgu.ac.kr>
2024-08-15 19:28:43 +0900

    [Feat] : 스케줄 DTO 추가

---
+++ src/main/java/side/onetime/dto/ScheduleDto.java
@@ -0,0 +1,37 @@
+package side.onetime.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public class ScheduleDto {
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class CreateDayScheduleRequest {
+        private String eventId;
+        private String memberId;
+        private List<DaySchedule> daySchedules;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class DaySchedule {
+        private String day;
+        private List<LocalTime> times;
+    }
+}
