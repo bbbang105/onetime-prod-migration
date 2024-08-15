b0532ee218e445ccbb2d6d0f8eeb6b7c41382379
bbbang105 <2018111366@dgu.ac.kr>
2024-08-15 18:34:59 +0900

    [Feat] : 멤버 로그인 & 중복 확인 API 구현

--- src/main/java/side/onetime/repository/MemberRepository.java
+++ src/main/java/side/onetime/repository/MemberRepository.java
@@ -1,7 +1,13 @@
 package side.onetime.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import side.onetime.domain.Event;
 import side.onetime.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    Boolean existsByEventAndNameAndPin(Event event, String name, String pin);
+    Boolean existsByEventAndName(Event event, String name);
+    Optional<Member> findByEventAndNameAndPin(Event event, String name, String pin);
 }
