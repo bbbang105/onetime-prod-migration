d254c29d43ee6cdb179ab97291f4a20b570dcc6c
bbbang105 <2018111366@dgu.ac.kr>
2024-08-15 14:40:19 +0900

    [Feat] : 전역 에러 처리 기능 추가

---
+++ src/main/java/side/onetime/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package side.onetime.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import side.onetime.global.common.ApiResponse;
+import side.onetime.global.common.code.BaseErrorCode;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    // Event
+    @ExceptionHandler(EventException.class)
+    public ResponseEntity<ApiResponse<BaseErrorCode>> handleTokenException(EventException e) {
+        EventErrorResult errorResult = e.getEventErrorResult();
+        return ApiResponse.onFailure(errorResult);
+    }
+}
