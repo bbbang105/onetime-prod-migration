a102d8b0aa1d4561ca2110817aaae423f3245e72
bbbang105 <2018111366@dgu.ac.kr>
2024-08-21 02:05:48 +0900

    #5 [feat] : 가장 많이 되는 시간 조회 성공 코드 추가

--- src/main/java/side/onetime/global/common/constant/SuccessStatus.java
+++ src/main/java/side/onetime/global/common/constant/SuccessStatus.java
@@ -16,6 +16,7 @@ public enum SuccessStatus implements BaseCode {
     _CREATED_EVENT(HttpStatus.CREATED, "201", "이벤트 생성에 성공했습니다."),
     _GET_EVENT(HttpStatus.OK, "200", "이벤트 조회에 성공했습니다."),
     _GET_PARTICIPANTS(HttpStatus.OK, "200", "참여자 조회에 성공했습니다."),
+    _GET_MOST_POSSIBLE_TIME(HttpStatus.OK, "200", "가장 많이 되는 시간 조회에 성공했습니다."),
     // Member
     _REGISTER_MEMBER(HttpStatus.CREATED, "201", "멤버 등록에 성공했습니다."),
     _LOGIN_MEMBER(HttpStatus.OK, "200", "멤버 로그인에 성공했습니다."),
