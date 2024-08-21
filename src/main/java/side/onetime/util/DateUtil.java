e02cedd6e1e8e48f9b2fba866caf33fb8c9f52cc
bbbang105 <2018111366@dgu.ac.kr>
2024-08-21 17:19:54 +0900

    #18 [feat] : 최적 시간대 정렬 기능 추가

--- src/main/java/side/onetime/util/DateUtil.java
+++ src/main/java/side/onetime/util/DateUtil.java
@@ -2,6 +2,8 @@ package side.onetime.util;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import side.onetime.dto.EventDto;
+import side.onetime.global.common.constant.Category;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -19,7 +21,6 @@ public class DateUtil {
     public List<LocalTime> createTimeSets(String start, String end) {
         List<LocalTime> timeSets = new ArrayList<>();
 
-        // "24:00"을 처리하기 위해 endTime을 "23:59"로 변환
         boolean isEndTimeMidnight = end.equals("24:00");
         if (isEndTimeMidnight) {
             end = "23:59";
@@ -29,13 +30,11 @@ public class DateUtil {
         LocalTime endTime = LocalTime.parse(end);
         LocalTime currentTime = startTime;
 
-        // 30분 단위로 시간을 추가
         while (!currentTime.isAfter(endTime.minusMinutes(30))) {
             timeSets.add(currentTime);
             currentTime = currentTime.plusMinutes(30);
         }
 
-        // endTime이 "24:00"인 경우 마지막 "23:30" 추가
         if (isEndTimeMidnight) {
             timeSets.add(LocalTime.of(23, 30));
         }
@@ -74,6 +73,48 @@ public class DateUtil {
                 .filter(day -> day != null && !day.isEmpty())
                 .distinct()
                 .sorted(Comparator.comparingInt(dayOrderMap::get))
+                .toList();
+    }
+
+    // 최적 시간대 리스트를 날짜 또는 요일별로 정렬
+    public List<EventDto.GetMostPossibleTime> sortMostPossibleTimes(List<EventDto.GetMostPossibleTime> mostPossibleTimes, Category category) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+
+        List<String> sortedList;
+        if (category.equals(Category.DAY)) {
+            // 요일로 정렬
+            List<String> dayOrder = Arrays.asList("일", "월", "화", "수", "목", "금", "토");
+            Map<String, Integer> dayOrderMap = IntStream.range(0, dayOrder.size())
+                    .boxed()
+                    .collect(Collectors.toMap(dayOrder::get, i -> i));
+
+            sortedList = mostPossibleTimes.stream()
+                    .map(EventDto.GetMostPossibleTime::getTimePoint)
+                    .filter(dayOrderMap::containsKey)
+                    .sorted(Comparator.comparingInt(dayOrderMap::get))
+                    .distinct()
+                    .toList();
+        } else {
+            // 날짜로 정렬
+            sortedList = mostPossibleTimes.stream()
+                    .map(EventDto.GetMostPossibleTime::getTimePoint)
+                    .filter(timePoint -> {
+                        try {
+                            LocalDate.parse(timePoint, dateFormatter);
+                            return true;
+                        } catch (DateTimeParseException e) {
+                            return false;
+                        }
+                    })
+                    .sorted(Comparator.comparing(timePoint -> LocalDate.parse(timePoint, dateFormatter)))
+                    .distinct()
+                    .toList();
+        }
+
+        // 정렬된 리스트에 따라 `EventDto.GetMostPossibleTime` 객체 정렬
+        return mostPossibleTimes.stream()
+                .filter(mostPossibleTime -> sortedList.contains(mostPossibleTime.getTimePoint()))
+                .sorted(Comparator.comparingInt(mostPossibleTime -> sortedList.indexOf(mostPossibleTime.getTimePoint())))
                 .toList();
     }
 }
