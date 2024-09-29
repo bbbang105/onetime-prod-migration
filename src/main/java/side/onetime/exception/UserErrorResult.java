d0c53f2a534041a7b7fc65d570e9c0ae18329d6e
bbbang105 <2018111366@dgu.ac.kr>
2024-09-30 06:02:56 +0900

    #37 [feat] : 닉네임 요청 값이 없을 시 에러를 반환한다

--- src/main/java/side/onetime/exception/UserErrorResult.java
+++ src/main/java/side/onetime/exception/UserErrorResult.java
@@ -10,7 +10,8 @@ import side.onetime.global.common.dto.ErrorReasonDto;
 @RequiredArgsConstructor
 public enum UserErrorResult implements BaseErrorCode {
     _NOT_FOUND_USER(HttpStatus.NOT_FOUND, "404", "유저를 찾을 수 없습니다."),
-    _NICKNAME_TOO_LONG(HttpStatus.BAD_REQUEST, "400", "닉네임 길이 제한을 초과했습니다.")
+    _NICKNAME_TOO_LONG(HttpStatus.BAD_REQUEST, "400", "닉네임 길이 제한을 초과했습니다."),
+    _NOT_FOUND_REQUEST_NICKNAME(HttpStatus.BAD_REQUEST, "400", "닉네임 요청 값을 찾을 수 없습니다.")
     ;
 
     private final HttpStatus httpStatus;
