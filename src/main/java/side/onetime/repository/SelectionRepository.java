311415aacbd55a39eec18f3b7978a5be4776b0e3
bbbang105 <2018111366@dgu.ac.kr>
2024-10-01 03:25:44 +0900

    #52 [feat] : 로그인 유저가 스케줄을 등록할 수 있다

--- src/main/java/side/onetime/repository/SelectionRepository.java
+++ src/main/java/side/onetime/repository/SelectionRepository.java
@@ -3,14 +3,13 @@ package side.onetime.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import side.onetime.domain.Event;
-import side.onetime.domain.Member;
-import side.onetime.domain.Selection;
+import side.onetime.domain.*;
 
 import java.util.List;
 
 public interface SelectionRepository extends JpaRepository<Selection, Long> {
     void deleteAllByMember(Member member);
+    void deleteAllByUserAndScheduleIn(User user, List<Schedule> schedules);
     @Query("SELECT s FROM Selection s JOIN FETCH s.schedule sc WHERE sc.event = :event")
     List<Selection> findAllSelectionsByEvent(@Param("event") Event event);
 }
