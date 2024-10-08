147a81b751f115cc8e23bf9ab99e47f4244e40a4
bbbang105 <2018111366@dgu.ac.kr>
2024-10-08 14:57:13 +0900

    #71 [feat] : 제한 개수를 넘어갈 시 에러를 반환한다

--- src/main/java/side/onetime/exception/TokenErrorResult.java
+++ src/main/java/side/onetime/exception/TokenErrorResult.java
@@ -12,7 +12,7 @@ public enum TokenErrorResult implements BaseErrorCode {
     _INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "401", "유효하지 않은 토큰입니다."),
     _INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "401", "유효하지 않은 리프레쉬 토큰입니다."),
     _EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "401", "만료된 토큰입니다."),
-    _NOT_FOUND_REFRESH_TOKEN(HttpStatus.NOT_FOUND, "404", "리프레쉬 토큰을 찾을 수 없습니다.")
+    _NOT_FOUND_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "401", "리프레쉬 토큰을 찾을 수 없습니다.")
     ;
 
     private final HttpStatus httpStatus;
