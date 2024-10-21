2a7df7f5cf5642564af4205a0177229ddace0677
bbbang105 <2018111366@dgu.ac.kr>
2024-10-22 00:04:13 +0900

    #82 [refactor] : URL DTO를 레코드로 리팩토링한다

--- src/main/java/side/onetime/service/UrlService.java
+++ src/main/java/side/onetime/service/UrlService.java
@@ -2,7 +2,10 @@ package side.onetime.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import side.onetime.dto.UrlDto;
+import side.onetime.dto.url.request.ConvertToOriginalUrlRequest;
+import side.onetime.dto.url.request.ConvertToShortenUrlRequest;
+import side.onetime.dto.url.response.ConvertToOriginalUrlResponse;
+import side.onetime.dto.url.response.ConvertToShortenUrlResponse;
 import side.onetime.exception.EventErrorResult;
 import side.onetime.exception.EventException;
 import side.onetime.repository.EventRepository;
@@ -16,20 +19,20 @@ public class UrlService {
     private final EventRepository eventRepository;
 
     // URL 단축 메서드
-    public UrlDto.ConvertToShortenUrlResponse convertToShortenUrl(UrlDto.ConvertToShortenUrlRequest convertToShortenUrlRequest) {
-        String originalUrl = convertToShortenUrlRequest.getOriginalUrl();
+    public ConvertToShortenUrlResponse convertToShortenUrl(ConvertToShortenUrlRequest convertToShortenUrlRequest) {
+        String originalUrl = convertToShortenUrlRequest.originalUrl();
 
         UUID eventId = extractEventIdFromUrl(originalUrl);
         if (!eventRepository.existsByEventId(eventId)) {
             throw new EventException(EventErrorResult._NOT_FOUND_EVENT);
         }
 
-        return UrlDto.ConvertToShortenUrlResponse.of(Base62Util.convertToShortenUrl(originalUrl));
+        return ConvertToShortenUrlResponse.of(Base62Util.convertToShortenUrl(originalUrl));
     }
 
     // URL 복원 메서드
-    public UrlDto.ConvertToOriginalUrlResponse convertToOriginalUrl(UrlDto.ConvertToOriginalUrlRequest convertToOriginalUrlRequest) {
-        String shortenUrl = convertToOriginalUrlRequest.getShortenUrl();
+    public ConvertToOriginalUrlResponse convertToOriginalUrl(ConvertToOriginalUrlRequest convertToOriginalUrlRequest) {
+        String shortenUrl = convertToOriginalUrlRequest.shortenUrl();
         String originalUrl = Base62Util.convertToOriginalUrl(shortenUrl);
 
         UUID eventId = extractEventIdFromUrl(originalUrl);
@@ -37,7 +40,7 @@ public class UrlService {
             throw new EventException(EventErrorResult._NOT_FOUND_EVENT);
         }
 
-        return UrlDto.ConvertToOriginalUrlResponse.of(originalUrl);
+        return ConvertToOriginalUrlResponse.of(originalUrl);
     }
 
     // URL에서 Event ID 추출
