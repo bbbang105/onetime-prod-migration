0e082f02d15635caf153e3590a4a5203768856cb
bbbang105 <2018111366@dgu.ac.kr>
2024-10-21 19:00:27 +0900

    #76 : [style] : 메서드명을 변경한다

--- src/main/java/side/onetime/repository/custom/EventRepositoryCustom.java
+++ src/main/java/side/onetime/repository/custom/EventRepositoryCustom.java
@@ -3,5 +3,5 @@ package side.onetime.repository.custom;
 import side.onetime.domain.Event;
 
 public interface EventRepositoryCustom {
-    void deleteUserEvent(Event event);
+    void deleteEvent(Event event);
 }
