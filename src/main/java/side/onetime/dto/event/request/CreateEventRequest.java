b9a39714b5b900ffd0cb035dd62584ae06f71128
bbbang105 <2018111366@dgu.ac.kr>
2024-10-21 20:22:21 +0900

    #82 [feat] : 유효성 검증을 위한 어노테이션을 추가한다

--- src/main/java/side/onetime/dto/event/request/CreateEventRequest.java
+++ src/main/java/side/onetime/dto/event/request/CreateEventRequest.java
@@ -3,6 +3,7 @@ package side.onetime.dto.event.request;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
 import side.onetime.domain.Event;
 import side.onetime.domain.enums.Category;
 
@@ -12,11 +13,11 @@ import java.util.UUID;
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record CreateEventRequest(
-        String title,
-        String startTime,
-        String endTime,
-        Category category,
-        List<String> ranges
+        @NotBlank(message = "제목은 필수 값입니다.") String title,
+        @NotBlank(message = "시작 시간은 필수 값입니다.") String startTime,
+        @NotBlank(message = "종료 시간은 필수 값입니다.") String endTime,
+        @NotBlank(message = "카테고리는 필수 값입니다.") Category category,
+        @NotBlank(message = "설문 범위는 필수 값입니다.") List<String> ranges
 ) {
     public Event toEntity() {
         return Event.builder()
