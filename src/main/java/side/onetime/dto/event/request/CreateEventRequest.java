2f472397441b12385ec9365d0979729fb2df9ce0
bbbang105 <2018111366@dgu.ac.kr>
2024-10-21 20:24:30 +0900

    #82 [refactor] : NotNull로 변경한다

--- src/main/java/side/onetime/dto/event/request/CreateEventRequest.java
+++ src/main/java/side/onetime/dto/event/request/CreateEventRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import side.onetime.domain.Event;
 import side.onetime.domain.enums.Category;
 
@@ -17,7 +18,7 @@ public record CreateEventRequest(
         @NotBlank(message = "시작 시간은 필수 값입니다.") String startTime,
         @NotBlank(message = "종료 시간은 필수 값입니다.") String endTime,
         @NotBlank(message = "카테고리는 필수 값입니다.") Category category,
-        @NotBlank(message = "설문 범위는 필수 값입니다.") List<String> ranges
+        @NotNull(message = "설문 범위는 필수 값입니다.") List<String> ranges
 ) {
     public Event toEntity() {
         return Event.builder()
