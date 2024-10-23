8ebbf2d04b7f8f809ecaaf81e35f08a6c59876c5
bbbang105 <2018111366@dgu.ac.kr>
2024-10-23 19:19:38 +0900

    #73 [feat] : 고정 스케줄을 등록할 수 있다

---
+++ src/main/java/side/onetime/dto/fixedEvent/response/FixedScheduleResponse.java
@@ -0,0 +1,14 @@
+package side.onetime.dto.fixedEvent.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.util.List;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record FixedScheduleResponse(
+        String timePoint,
+        List<String> times
+) {}
