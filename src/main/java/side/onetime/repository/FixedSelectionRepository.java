16f4746fc17550676b5ed5c32ad90b79aea8f918
bbbang105 <2018111366@dgu.ac.kr>
2024-10-24 00:45:32 +0900

    #90 [feat] : 고정 선택 스케줄을 삭제하는 기능을 추가한다

--- src/main/java/side/onetime/repository/FixedSelectionRepository.java
+++ src/main/java/side/onetime/repository/FixedSelectionRepository.java
@@ -2,6 +2,7 @@ package side.onetime.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import side.onetime.domain.FixedSelection;
+import side.onetime.repository.custom.FixedSelectionRepositoryCustom;
 
-public interface FixedSelectionRepository extends JpaRepository<FixedSelection, Long> {
+public interface FixedSelectionRepository extends JpaRepository<FixedSelection, Long>, FixedSelectionRepositoryCustom {
 }
