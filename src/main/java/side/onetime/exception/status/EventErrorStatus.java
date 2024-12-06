ffe1ed997883ab516c6801848341dc6fdcf85be7
bbbang105 <2018111366@dgu.ac.kr>
2024-12-07 01:10:35 +0900

    #127 [feat] : 에러 처리 및 주석을 추가한다

--- src/main/java/side/onetime/exception/status/EventErrorStatus.java
+++ src/main/java/side/onetime/exception/status/EventErrorStatus.java
@@ -11,7 +11,9 @@ import side.onetime.global.common.dto.ErrorReasonDto;
 public enum EventErrorStatus implements BaseErrorCode {
     _NOT_FOUND_EVENT(HttpStatus.NOT_FOUND, "EVENT-001", "이벤트를 찾을 수 없습니다."),
     _IS_NOT_DATE_FORMAT(HttpStatus.BAD_REQUEST, "EVENT-002", "날짜 이벤트에 요일을 입력할 수 없습니다."),
-    _IS_NOT_DAY_FORMAT(HttpStatus.BAD_REQUEST, "EVENT-003", "요일 이벤트에 날짜를 입력할 수 없습니다.")
+    _IS_NOT_DAY_FORMAT(HttpStatus.BAD_REQUEST, "EVENT-003", "요일 이벤트에 날짜를 입력할 수 없습니다."),
+    _NOT_FOUND_EVENT_QR_CODE(HttpStatus.NOT_FOUND, "EVENT-004", "이벤트 QR 코드를 찾을 수 없습니다."),
+    _FAILED_GENERATE_QR_CODE(HttpStatus.INTERNAL_SERVER_ERROR, "EVENT-005", "QR 코드를 생성하고 업로드 하는 과정에서 문제가 발생했습니다."),
     ;
 
     private final HttpStatus httpStatus;
