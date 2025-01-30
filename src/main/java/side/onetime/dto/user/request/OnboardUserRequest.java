b029f77db984e76e0962966d12677b1403ab4a6d
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-01-31 03:18:52 +0900

    [feat] : 유저 온보딩 과정에 약관 동의 & 수면 시간 등록 로직을 추가한다 (#171)

--- src/main/java/side/onetime/dto/user/request/OnboardUserRequest.java
+++ src/main/java/side/onetime/dto/user/request/OnboardUserRequest.java
@@ -4,11 +4,24 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record OnboardUserRequest(
-        @NotBlank(message = "Register Token은 필수 값입니다.") String registerToken,
-        @NotBlank(message = "Nickname은 필수 값입니다.") String nickname
+        @NotBlank(message = "레지스터 토큰은 필수 값입니다.")
+        String registerToken,
+        @NotBlank(message = "닉네임은 필수 값입니다.")
+        String nickname,
+        @NotNull(message = "서비스 이용약관 동의여부는 필수 값입니다.")
+        Boolean servicePolicyAgreement,
+        @NotNull(message = "개인정보 수집 및 이용 동의여부는 필수 값입니다.")
+        Boolean privacyPolicyAgreement,
+        @NotNull(message = "마케팅 정보 수신 동의여부는 필수 값입니다.")
+        Boolean marketingPolicyAgreement,
+        @NotBlank(message = "수면 시작 시간은 필수 값입니다.")
+        String sleepStartTime,
+        @NotBlank(message = "수면 종료 시간은 필수 값입니다.")
+        String sleepEndTime
 ) {
 }
