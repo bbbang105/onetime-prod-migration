371fb2e4eb1dd590e6ddcd6b419161e6a37262a7
bbbang105 <2018111366@dgu.ac.kr>
2025-02-23 17:08:36 +0900

    [feat] : OPTIONS 요청은 모두 허용한다

--- src/main/java/side/onetime/global/config/SecurityConfig.java
+++ src/main/java/side/onetime/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package side.onetime.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -100,6 +101,7 @@ public class SecurityConfig {
                 .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(SWAGGER_URLS).permitAll()
                         .requestMatchers(PUBLIC_URLS).permitAll()
                         .requestMatchers(AUTHENTICATED_URLS).authenticated()
