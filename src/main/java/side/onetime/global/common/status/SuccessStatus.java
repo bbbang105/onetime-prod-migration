8cb7a30791f8dc431b541c936aae8c938a63b6bd
bbbang105 <2018111366@dgu.ac.kr>
2025-03-24 05:25:51 +0900

    #200 [feat] : 관리자는 탈퇴할 수 있다

--- src/main/java/side/onetime/global/common/status/SuccessStatus.java
+++ src/main/java/side/onetime/global/common/status/SuccessStatus.java
@@ -59,6 +59,7 @@ public enum SuccessStatus implements BaseCode {
     _GET_ADMIN_USER_PROFILE(HttpStatus.OK, "200", "관리자 프로필 조회에 성공했습니다."),
     _GET_ALL_ADMIN_USER_DETAIL(HttpStatus.OK, "200", "전체 관리자 정보 조회에 성공했습니다."),
     _UPDATE_ADMIN_USER_STATUS(HttpStatus.OK, "200", "관리자 권한 수정에 성공했습니다."),
+    _WITHDRAW_ADMIN_USER(HttpStatus.OK, "200", "관리자 계정 탈퇴에 성공했습니다."),
     ;
 
     private final HttpStatus httpStatus;
