e0534cd07a896dbebb5998f062d9e7c6f2ad27b3
bbbang105 <2018111366@dgu.ac.kr>
2025-05-21 19:48:54 +0900

    #233 [refactor] : 중복 메서드를 분리하고 fetch join을 적용한다

--- src/main/java/side/onetime/repository/SelectionRepository.java
+++ src/main/java/side/onetime/repository/SelectionRepository.java
@@ -8,10 +8,37 @@ import side.onetime.domain.*;
 import java.util.List;
 
 public interface SelectionRepository extends JpaRepository<Selection, Long> {
+
     void deleteAllByMember(Member member);
+
     void deleteAllByUserAndScheduleIn(User user, List<Schedule> schedules);
-    @Query("SELECT s FROM Selection s JOIN FETCH s.schedule sc WHERE sc.event = :event")
+
+    @Query("""
+        SELECT s FROM Selection s
+        JOIN FETCH s.schedule sc
+        WHERE sc.event = :event
+    """)
     List<Selection> findAllSelectionsByEvent(@Param("event") Event event);
-    @Query("SELECT COUNT(s) > 0 FROM Selection s WHERE s.user = :user AND s.schedule.event = :event")
+
+    @Query("""
+        SELECT COUNT(s) > 0 FROM Selection s
+        WHERE s.user = :user
+        AND s.schedule.event = :event
+    """)
     boolean existsByUserAndEventSchedules(@Param("user") User user, @Param("event") Event event);
+
+    @Query("""
+        SELECT s FROM Selection s
+        JOIN FETCH s.schedule sc
+        WHERE s.member = :member
+    """)
+    List<Selection> findAllByMemberWithSchedule(@Param("member") Member member);
+
+    @Query("""
+        SELECT s FROM Selection s
+        JOIN FETCH s.schedule sc
+        JOIN FETCH sc.event
+        WHERE s.user = :user
+    """)
+    List<Selection> findAllByUserWithScheduleAndEvent(@Param("user") User user);
 }
