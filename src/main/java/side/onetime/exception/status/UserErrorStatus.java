e03c4b376a8dea51dbcac91e7dee8d7a046ac52e
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-06-18 15:19:48 +0900

    [fix] : 참여 이벤트 존재 시, 유저 탈퇴가 되지 않는 문제를 해결한다 (#250)
    * #249 [fix] : 참여 이벤트 존재 시, 유저 탈퇴가 되지 않는 문제를 해결한다
    
    * #249 [fix] : 이벤트 참여여부도 제거하도록 변경한다
    
    * #249 [feat] : distinct()를 붙여 중복 id 로딩을 방지한다
    
    * #249 [feat] : 유저 탈퇴 시, 관련한 리프레쉬 토큰을 모두 제거한다
    
    * #249 [feat] : JWT 관련 에러는 401로 통일한다

--- src/main/java/side/onetime/exception/status/UserErrorStatus.java
+++ src/main/java/side/onetime/exception/status/UserErrorStatus.java
@@ -10,8 +10,8 @@ import side.onetime.global.common.dto.ErrorReasonDto;
 @RequiredArgsConstructor
 public enum UserErrorStatus implements BaseErrorCode {
     _NOT_FOUND_USER(HttpStatus.NOT_FOUND, "USER-001", "유저를 찾을 수 없습니다."),
-    _NOT_FOUND_USER_BY_USERNAME(HttpStatus.NOT_FOUND, "USER-002", "username으로 user를 찾을 수 없습니다."),
-    _NOT_FOUND_USER_BY_USERID(HttpStatus.NOT_FOUND, "USER-003", "userId로 user를 찾을 수 없습니다."),
+    _NOT_FOUND_USER_BY_USERNAME(HttpStatus.UNAUTHORIZED, "USER-002", "username으로 user를 찾을 수 없습니다."),
+    _NOT_FOUND_USER_BY_USERID(HttpStatus.UNAUTHORIZED, "USER-003", "userId로 user를 찾을 수 없습니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "USER-004", "인증된 사용자가 아닙니다."),
     ;
 
